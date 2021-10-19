3cbd26fa68ef39be1d9e2d87c7e153b4b255af43
Santiago Serrano <santiagoandressp@ufps.edu.co>
2021-10-19 11:55:24 -0500

    Update empresa.service.ts

--- src/app/services/empresa.service.ts
+++ src/app/services/empresa.service.ts
@@ -1,18 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import * as global from 'global'
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmpresaService {
 
-  uri = "http://localhost:8080/empresa";
+  uri=`${global.url}/paquete/`;
   constructor(private http: HttpClient) { }
 
   public listarEmpresa():Observable<any>{
     return this.http.get<any>(this.uri);
   }
-  
+
 
 }
